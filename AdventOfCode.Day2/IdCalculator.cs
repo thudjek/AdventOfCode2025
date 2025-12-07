630aeb98114fc70efde90b237a97c11b8af4d935
Tomislav Huđek <thudjek@gmail.com>
2025-12-07 11:41:39 +0100

    Day2

---
+++ AdventOfCode.Day2/IdCalculator.cs
@@ -0,0 +1,117 @@
+﻿namespace AdventOfCode.Day2;
+
+public class IdCalculator
+{
+    public static long CalculateResultPart1(List<long> inputNumbers)
+    {
+        long result = 0;
+
+        foreach (var inputNumber in inputNumbers)
+        {
+            var numAsString = inputNumber.ToString();
+
+            if (numAsString.Length % 2 != 0)
+            {
+                continue;
+            }
+
+            var firstPart = numAsString.AsSpan(0, numAsString.Length / 2);
+            var secondPart = numAsString.AsSpan(numAsString.Length / 2);
+
+            if (firstPart.SequenceEqual(secondPart))
+            {
+                result += inputNumber;
+            }
+        }
+
+        return result;
+    }
+
+    public static long CalculateResultPart2(List<long> inputNumbers)
+    {
+        long result = 0;
+
+        foreach (var inputNumber in inputNumbers)
+        {
+            var numAsString = inputNumber.ToString();
+
+            if (numAsString.Length == 1)
+            {
+                continue;
+            }
+
+            if (IsNumberWithSameDigits(numAsString))
+            {
+                result += inputNumber;
+                continue;
+            }
+
+            var divisors = GetAllDivisors(numAsString.Length);
+
+            if (divisors.Count == 0)
+            {
+                continue;
+            }
+
+            foreach (var divisor in divisors)
+            {
+                if (DoesNumberContainSameParts(numAsString, divisor))
+                {
+                    result += inputNumber;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsNumberWithSameDigits(string numAsString)
+    {
+        var firstChar = numAsString[0];
+
+        for (var i = 1; i < numAsString.Length; i++)
+        {
+            if (numAsString[i] != firstChar)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool DoesNumberContainSameParts(string numAsString, int numOfParts)
+    {
+        var lengthOfOnePart = numAsString.Length / numOfParts;
+        var firstPart = numAsString.AsSpan(0, lengthOfOnePart);
+        var nextStartIndex = lengthOfOnePart;
+
+        for (int i = 2; i <= numOfParts; i++)
+        {
+            var nextPart = numAsString.AsSpan(nextStartIndex, lengthOfOnePart);
+            nextStartIndex += lengthOfOnePart;
+
+            if (!firstPart.SequenceEqual(nextPart))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static List<int> GetAllDivisors(int num)
+    {
+        var result = new List<int>();
+        for (var i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ AdventOfCode.Day2/Program.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Day2;
+
+var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input_d2p1.txt");
+var inputText = File.ReadAllText(path);
+var allRanges = inputText.Split(',');
+var allNumbers = new List<long>();
+
+foreach (var range in allRanges)
+{
+    var numbersAsString = range.Split('-');
+
+    if (!long.TryParse(numbersAsString[0], out var firstNum) || !long.TryParse(numbersAsString[1], out var secondNum))
+    {
+        throw new InvalidDataException("Invalid input data.");
+    }
+
+    for (var i = firstNum; i <= secondNum; i++)
+    {
+        allNumbers.Add(i);
+    }
+}
+
+//Console.WriteLine(IdCalculator.CalculateResultPart1(allNumbers));
+Console.WriteLine(IdCalculator.CalculateResultPart2(allNumbers));
+
+Console.ReadLine();
+
