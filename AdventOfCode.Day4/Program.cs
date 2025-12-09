11c4269415fe5fb7eddb0be1e237ee5a77abd21a
Tomislav Huđek <thudjek@gmail.com>
2025-12-09 16:25:11 +0100

    Day4

---
+++ AdventOfCode.Day4/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode.Day4;
+
+var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input_d4p1.txt");
+var inputLines = File.ReadAllLines(path);
+
+var rollsCalculator = new RollsCalculator(inputLines);
+
+Console.WriteLine(rollsCalculator.CalculateResultPart1());
+Console.WriteLine(rollsCalculator.CalculateResultPart2());
+
+Console.ReadLine();
---
+++ AdventOfCode.Day4/RollsCalculator.cs
@@ -0,0 +1,125 @@
+﻿namespace AdventOfCode.Day4;
+
+public class RollsCalculator
+{
+    private const char At = '@';
+    private const char Dot = '.';
+
+    private bool[,] currentGrid;
+    private bool[,] nextGrid;
+    private int lastRowIndex;
+    private int lastColumnIndex;
+
+    public RollsCalculator(string[] inputLines)
+    {
+        ParseInputLinesToGrid(inputLines);
+        lastRowIndex = currentGrid.GetLength(0) - 1;
+        lastColumnIndex = currentGrid.GetLength(1) - 1;
+    }
+
+    public int CalculateResultPart1()
+    {
+        var result = 0;
+
+        for (var row = 0; row <= lastRowIndex; row++)
+        {
+            for (var col = 0; col <= lastColumnIndex; col++)
+            {
+                if (!currentGrid[row, col])
+                {
+                    continue;
+                }
+
+                var adjacentRollNum = 0;
+
+                for (var i = -1; i <= 1; i++)
+                {
+                    for (var j = -1; j <= 1; j++)
+                    {
+                        if (i == 0 && j == 0)
+                            continue;
+
+                        if ((row + i < 0) || (row + i > lastRowIndex) || (col + j < 0) || (col + j > lastColumnIndex))
+                            continue;
+
+                        if (currentGrid[row + i, col + j])
+                            adjacentRollNum++;
+                    }
+                }
+
+                if (adjacentRollNum < 4)
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int CalculateResultPart2()
+    {
+        int iterationResult;
+        var totalResult = 0;
+
+        do
+        {
+            iterationResult = 0;
+
+            for (var row = 0; row <= lastRowIndex; row++)
+            {
+                for (var col = 0; col <= lastColumnIndex; col++)
+                {
+                    if (!currentGrid[row, col])
+                    {
+                        continue;
+                    }
+
+                    var adjacentRollNum = 0;
+
+                    for (var i = -1; i <= 1; i++)
+                    {
+                        for (var j = -1; j <= 1; j++)
+                        {
+                            if (i == 0 && j == 0)
+                                continue;
+
+                            if ((row + i < 0) || (row + i > lastRowIndex) || (col + j < 0) || (col + j > lastColumnIndex))
+                                continue;
+
+                            if (currentGrid[row + i, col + j])
+                                adjacentRollNum++;
+                        }
+                    }
+
+                    if (adjacentRollNum < 4)
+                    {
+                        iterationResult++;
+                        nextGrid[row, col] = false;
+                    }
+                }
+            }
+
+            totalResult += iterationResult;
+            currentGrid = nextGrid;
+
+        } while (iterationResult != 0);
+
+        return totalResult;
+    }
+
+    private void ParseInputLinesToGrid(string[] inputLines)
+    {
+        for (int i = 0; i < inputLines.Length; i++)
+        {
+            for (int j = 0; j < inputLines[i].Length; j++)
+            {
+                currentGrid ??= new bool[inputLines.Length, inputLines[i].Length];
+                nextGrid ??= new bool[inputLines.Length, inputLines[i].Length];
+
+                currentGrid[i, j] = inputLines[i][j] == At;
+                nextGrid[i, j] = inputLines[i][j] == At;
+            }
+        }
+    }
+}
